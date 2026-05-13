049619a7538719576079abaceb57ae23535dff79
Chiara Kappe <chiara@Chiaras-MacBook-Pro.local>
2026-05-14 01:49:52 +0200

    added function to check if nickname is valid

--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -16,3 +16,28 @@ std::string	UserProfile::source() const
 {
 	return nickname + "!" + username + "@" + hostname;
 }
+
+
+
+bool	Server::isValidNickname(const std::string& nickname) const
+{
+	if (nickname.empty())
+		return false;
+	if (nickname.size() > 30)
+		return false;
+	if (nickname[0] == '#' || nickname[0] == '&' || nickname[0] == ':'
+		|| nickname[0] == '@' || std::isdigit(nickname[0]) || std::isspace(nickname[0]))
+		return false;
+	if (nickname.size() < 3)
+		return false;
+	for (std::size_t i = 0; i < nickname.size(); ++i)
+	{
+		const char c = nickname[i];
+		if (!std::isalnum(c) && c != '\\' && c != '|'
+			&& c != '[' && c != ']' && c != '{'
+			&& c != '}' && c != '-' && c != '_')
+			return false;
+	}
+	return true;
+}
+
