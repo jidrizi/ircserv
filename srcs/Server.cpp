9eddfc16b8b1945f6a96e667067267493071e76c
jidrizi <jidrizi@student.42.fr>
2026-05-14 17:52:29 +0200

    made a small parser for the arguments at the start and a signal handler

---
+++ srcs/Server.cpp
@@ -0,0 +1,42 @@
+#include <cctype>
+#include <sstream>
+
+bool Server::stopSignal = false;
+
+Server::Server()
+	: port(0), serverSocketFd(-1)
+{
+}
+
+Server::~Server()
+{
+
+}
+
+int	printError(const std::string& errorMessage)
+{
+	std::cerr << "Error: " << errorMessage << std::endl;
+	return 1;
+}
+
+int	Server::parseArgs(char** argv)
+{
+	const int parsedPort = std::atoi(argv[1]);
+	if (parsedPort < 1024 || parsedPort > 65535)
+		return printError("invalid port");
+	port = parsedPort;
+
+	const std::string parsedPassword = argv[2];
+	if (parsedPassword.empty())
+		return printError("password cannot be empty");
+	if (parsedPassword.find(' ') != std::string::npos)
+		return printError("password cannot contain spaces");
+	password = parsedPassword;
+	return 0;
+}
+
+void	Server::signalHandler(int signalNumber)
+{
+	(void)signalNumber;
+	stopSignal = true;
+}
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,8 +1,24 @@
-#include "Server.hpp"
+#include "ft_irc.hpp"
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
+	Server server;
+
 	if (argc != 3)
 		return printError("usage: ./ircserv <port> <password>");
+	if (server.parseArgs(argv) != 0)
+		return 1;
+
+	try
+	{
+		signal(SIGINT, Server::signalHandler);
+		signal(SIGQUIT, Server::signalHandler);
+		// server.run();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
 	return 0;
 }
