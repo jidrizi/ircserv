bf974f4e5da1c1bd68e4f6a140b716e61f1039ec
Chiara Kappe <chiara@Chiaras-MacBook-Pro.local>
2026-05-16 16:03:31 +0200

    finished getters and utils for client and also pushed Nickname checksin Server.cpp, sorry for delay

--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -49,3 +49,46 @@ const UserProfile&	ClientSession::user() const
 	return userData;
 }
 
+std::string&	ClientSession::recvBuffer()
+{
+	return recvBufferData;
+}
+
+std::string&	ClientSession::sendBuffer()
+{
+	return sendBufferData;
+}
+
+const std::string&	ClientSession::sendBuffer() const
+{
+	return sendBufferData;
+}
+
+bool	ClientSession::hasPendingOutput() const
+{
+	return !sendBufferData.empty();
+}
+
+void	ClientSession::consumeSentBytes(std::size_t sentBytes)
+{
+	// Remove sentBytes from front of send buffer after successful write
+	sendBufferData.erase(0, sentBytes);
+}
+
+bool	ClientSession::popNextLine(std::string& line)
+{
+	std::size_t lineEnd = recvBufferData.find('\n');
+	if (lineEnd == std::string::npos)
+		return false;  // No complete line yet, wait for more data
+	
+	// Extract line from start to newline (not including the \n)
+	line = recvBufferData.substr(0, lineEnd);
+	
+	// Remove trailing \r if present (handle \r\n line endings)
+	if (!line.empty() && line[line.size() - 1] == '\r')
+		line.erase(line.size() - 1);
+	
+	// Remove processed line and newline from buffer
+	recvBufferData.erase(0, lineEnd + 1);
+	return true;  // Successfully extracted a complete line
+}
---
+++ srcs/Server.cpp
@@ -0,0 +1,36 @@
+#include "Server.hpp"
+#include "Client.hpp"
+
+bool	Server::isNicknameInUse(const std::string& nickname, int excludingFd) const
+{
+	for (std::vector<ClientSession*>::const_iterator it = clients.begin(); it != clients.end(); ++it)
+	{
+		if ((*it)->fd() == excludingFd)
+			continue;
+		if ((*it)->user().nickname == nickname)
+			return true;
+	}
+	return false;
+}
+
+bool	Server::isValidNickname(const std::string& nickname) const
+{
+	if (nickname.empty())
+		return false;
+	if (nickname.size() > 30)
+		return false;
+	if (nickname[0] == '#' || nickname[0] == '&' || nickname[0] == ':'
+		|| nickname[0] == '@' || std::isdigit(nickname[0]) || std::isspace(nickname[0]))
+		return false;
+	if (nickname.size() < 3)
+		return false;
+	for (std::size_t i = 0; i < nickname.size(); ++i)
+	{
+		const char c = nickname[i];
+		if (!std::isalnum(c) && c != '\\' && c != '|'
+			&& c != '[' && c != ']' && c != '{'
+			&& c != '}' && c != '-' && c != '_')
+			return false;
+	}
+	return true;
+}
